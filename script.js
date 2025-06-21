cce1537d2fa8631d43747fbba2d0a434d6eb7f4b
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2025-06-21 11:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let idx=0;
+	let idx1=0;
+	s1=s1.toLowerCase();
+	s2=s2.toUpperCase();
+	while(idx<s1.length()){
+		if(s1[idx]==s2[idx1]){
+			return idx;
+		}
+		idx++;
+		
+	}
+	return -1;
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
+//alert(indexOfIgnoreCase(s1, s2));
